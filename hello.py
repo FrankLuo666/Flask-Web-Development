9c1fcf7a5f887a52743fe2032443a753f404c517
FrankLuo <FrankLuo666@gmail.com>
2022-06-23 17:07:08 +0900

    add package

--- hello.py
+++ hello.py
@@ -28,13 +28,6 @@ def internal_server_error(e):
 
 
 @app.route("/", methods=["GET", "POST"])
-# def index():
-#     name = None
-#     form = NameForm()
-#     if form.validate_on_submit():
-#         name = form.name.data
-#         form.name.data = ""
-#     return render_template("index.html", form=form, name=name)
 def index():
     form = NameForm()
     if form.validate_on_submit():
