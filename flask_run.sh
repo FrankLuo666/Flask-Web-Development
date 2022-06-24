1fedec12a51084cd5129e442e659d289628f7361
FrankLuo <FrankLuo666@gmail.com>
2022-06-24 17:10:26 +0900

    send email by flask in app

--- flask_run.sh
+++ flask_run.sh
@@ -1,4 +1,8 @@
 export FLASK_APP=hello
 export FLASK_ENV=development
 
+#export MAIL_USERNAME=sender-usernmae
+#export MAIL_PASSWORD=sender-passowrd
+#export FLASKY_ADMIN=your-email-address
+
 flask run -p 8080
