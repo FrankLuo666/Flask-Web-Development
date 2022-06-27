908b12826a4bd09d04c32e7c2a4ce7a0e6c27571
FrankLuo <FrankLuo666@gmail.com>
2022-06-27 17:32:05 +0900

    use Large Application Structure

--- flask_run.sh
+++ flask_run.sh
@@ -1,8 +1,5 @@
-export FLASK_APP=hello
+export FLASK_APP=flasky
+export FLASK_DEBUG=1
 export FLASK_ENV=development
-
-#export MAIL_USERNAME=sender-usernmae
-#export MAIL_PASSWORD=sender-passowrd
-#export FLASKY_ADMIN=your-email-address
 
 flask run -p 8080
